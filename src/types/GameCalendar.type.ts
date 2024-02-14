d23de48a8f0096c617cb598a86e5b172ea2b0003
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-02-14 13:17:46 +0100

    create types folder to keeep types in one place

---
+++ src/types/GameCalendar.type.ts
@@ -0,0 +1,11 @@
+export type GameCalendarType = {
+  id: string;
+  gameType: string;
+  date: string;
+  firstTeamName: string;
+  firstTeamLogo: string;
+  firstTeamGoals: string;
+  secondTeamName: string;
+  secondTeamLogo: string;
+  secondTeamGoals: string;
+};
