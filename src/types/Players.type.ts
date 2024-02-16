2d5e2661f56e918986fe2870aed85e2c5c469015
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-02-16 15:23:50 +0100

    create player type

---
+++ src/types/Players.type.ts
@@ -0,0 +1,10 @@
+export type PlayersType = {
+  players: PlayerType[];
+};
+export type PlayerType = {
+  id: string;
+  name: string;
+  position: string;
+  jerseyNumber: string;
+  imgPlayer: string;
+};
