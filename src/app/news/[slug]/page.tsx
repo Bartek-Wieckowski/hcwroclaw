d7044488ce1904a98bad8064840e7b1d047c9ac5
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-02-16 11:30:35 +0100

    create and display data to single news page

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -1,6 +1,9 @@
 import { singleNewsType } from '@/types/News.type';
 import { API_URL } from '@/utils/constants';
 import axios from 'axios';
+import styles from './singleNewsPage.module.css';
+import Image from 'next/image';
+import { BiSolidStar } from 'react-icons/bi';
 
 type slugType = {
   slug: string;
@@ -21,12 +24,27 @@ export default async function SingleNews({ params }: { params: slugType }) {
   const singleNews = await getSingleNews(slug);
   return (
     <section>
-      <div className="container">{singleNews.map((news:singleNewsType)=>(
-        <div key={news.id}>
-            <h1>{news.title}</h1>
-            <h1>{news.author}</h1>
-        </div>
-      ))}</div>
+      <div className="container">
+        {singleNews.map((news: singleNewsType) => (
+          <article key={news.id} className={styles.singleNewsArticle}>
+            <h1 className={styles.singleNewsTitle}>{news.title}</h1>
+            <div className={styles.singleNewsDateAuthor}>
+              <small className={styles.singleNewsDate}>{news.date}</small>
+              <small className={styles.singleNewsAuthor}>
+                {news.author} <BiSolidStar />
+              </small>
+            </div>
+            <Image
+              src={news.img}
+              alt={news.title}
+              width="600"
+              height="300"
+              className={styles.singleNewsImg}
+            />
+            <p className={styles.singleNewsDesc}>{news.desc}</p>
+          </article>
+        ))}
+      </div>
     </section>
   );
 }
