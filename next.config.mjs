1293ec9efdc4a3e323303b9aa2db3d817177f493
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-02-16 15:20:40 +0100

    add another path allowed

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'images.leaguerepublic.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'assets.nhle.com',
+      },
     ],
   },
 };
