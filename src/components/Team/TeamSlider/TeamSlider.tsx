0afa943004613f25a61d1ca4fb83733f2e9a43a5
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-02-16 15:24:22 +0100

    create two components slider and team list

---
+++ src/components/Team/TeamSlider/TeamSlider.tsx
@@ -0,0 +1,41 @@
+'use client';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Scrollbar, A11y, Pagination } from 'swiper/modules';
+import Image from 'next/image';
+import styles from './teamSlider.module.css';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import { teamImagesSlider } from './teamImagesSlider-data';
+
+function TeamSlider() {
+  return (
+    <div className={styles.teamSliderWrapper}>
+      <Swiper
+        modules={[Scrollbar, A11y, Pagination]}
+        slidesPerView={1}
+        pagination={{
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+          },
+        }}
+        scrollbar={{ draggable: true }}
+        autoHeight={true}
+        className={styles.teamSliderSwiper}
+      >
+        {teamImagesSlider.map((imgSlider) => (
+          <SwiperSlide key={imgSlider.id} className={styles.sliderSwiperSlide}>
+            <Image
+              src={imgSlider.path}
+              alt={imgSlider.title}
+              className={styles.teamSliderImage}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
+
+export default TeamSlider;
