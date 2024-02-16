9fa7206ac695e00da8b74daf568bbbaf5f124b89
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-02-16 10:31:31 +0100

    create two function to cut text

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -13,6 +13,31 @@ export function formatDate(dateString: string) {
     'LIS',
     'GRU',
   ];
- const [year, month, day] = dateString.split('-');
- return `${parseInt(day, 10)} ${months[parseInt(month, 10) - 1]}`;
+  const [year, month, day] = dateString.split('-');
+  return `${parseInt(day, 10)} ${months[parseInt(month, 10) - 1]}`;
+}
+
+export function convertDate(dateString: string): Date {
+  const parts = dateString.split('-');
+  const formattedDate = `${parts[2]}-${parts[1]}-${parts[0]}`;
+  return new Date(formattedDate);
+}
+
+export function findDotAndCutText(text: string) {
+  const dotIndex = text.indexOf('.');
+  if (dotIndex !== -1) {
+    return text.substring(0, dotIndex + 1) + '..';
+  }
+  return '';
+}
+
+export function truncateText(text: string) {
+  const words: string[] = text.split(' ');
+
+  if (words.length > 7) {
+    const truncatedText: string = words.slice(0, 7).join(' ');
+    return truncatedText + '...';
+  } else {
+    return text;
+  }
 }
